0bef21e13af6ee3b6e649cd132b87cc449e3b453
“Sameer <dcoolsam@gmail.com>
2025-08-13 11:23:10 +0100

    Refactored explorepage to include new style of locators

--- pages/explore_page.py
+++ pages/explore_page.py
@@ -1,13 +1,15 @@
 from pages.base_page import BasePage
 
 class ExplorePage(BasePage):
-    # Locators
-    PAGE_TITLE = 'h1, .page-title'
-    
+    def __init__(self, page):
+        super().__init__(page)
+        # Locators
+        self.page_title = page.locator('h1, .page-title')
+
     def get_page_title(self) -> str:
         """Get page title"""
-        return self.get_text(self.PAGE_TITLE)
-    
+        return self.page_title.text_content()
+
     def is_loaded(self) -> bool:
         """Check if explore page is loaded"""
         return "/explore" in self.page.url
