15303401e0892f6ab049063ba97cfec52ef96c44
“Sameer <dcoolsam@gmail.com>
2025-08-13 11:24:45 +0100

    Refactored settings page to include new style of locators

--- pages/settings_page.py
+++ pages/settings_page.py
@@ -1,26 +1,25 @@
 from pages.base_page import BasePage
+from config.config import Config
 
 class SettingsPage(BasePage):
-    # Locators
-    PRIVATE_EQUITY_CHECKBOX = "text=Profile pages"
+    def __init__(self, page):
+        super().__init__(page)
+        # Locators
+        self.private_equity_section = page.get_by_text("Profile pages)
 
     def navigate(self):
         """Navigate to settings page"""
-        from config.config import Config
-        
-        # Let Login complete before moving to settings 
-        self.page.wait_for_url(f"{Config.BASE_URL}/insights", timeout=20000)  # waits up to 20 seconds
-        
-        #Load Settings
+        # Wait for login to complete before moving to settings
+        self.page.wait_for_url(f"{Config.BASE_URL}/insights", timeout=20000)
+
+        # Load Settings page
         settings_url = f"{Config.BASE_URL}/account/settings"
         self.navigate_to(settings_url)
-    
+
     def is_checkbox_checked(self, checkbox_label: str) -> bool:
         """Check if a specific checkbox is checked"""
         # Scope locator to the 'Profile pages' section
-        profile_section = self.page.locator(self.PRIVATE_EQUITY_CHECKBOX).locator("..") 
-        checkbox = profile_section.get_by_label(checkbox_label)
+        checkbox = self.private_equity_section.locator("..").get_by_label(checkbox_label)
         if checkbox:
-            # print(f"Checkbox with label '{checkbox_label}'  found")
             return checkbox.is_checked()
-
+        return False
